6d7c05c452fb8dacaeda7cc278f241ba03f925ff
Abbet Wang <abbetwang@gmail.com>
2019-04-02 23:53:15 +0800

    alter decorate

--- src/app/decorator/router.ts
+++ src/app/decorator/router.ts
@@ -1,4 +1,4 @@
-import * as Router from 'koa-router'
+import * as Router from 'express-router'
 import * as dir from 'dir_filenames'
 import { resolve } from 'path'
 import { Middleware } from '../../type'
