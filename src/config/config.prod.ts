59684da566f5005583de02cffd9de363abed7651
AlfieriChou <alfierichou@gmail.com>
2019-04-02 15:10:58 +0800

    config module

---
+++ src/config/config.prod.ts
@@ -0,0 +1,11 @@
+import { Config } from '../type'
+
+export const config: Config = {
+  port: 3000,
+  mysql: {
+    host: '140.143.194.238',
+    user: 'istar',
+    password: 'iStar12345@',
+    database: 'sofo'
+  }
+}
--- src/type.d.ts
+++ src/type.d.ts
@@ -1,1 +1,15 @@
 export type Middleware = (ctx: Koa.Context, next?: any) => any | Promise<any>
+
+export interface Config {
+  env?: string
+  port: number
+  mysql: Mysql
+}
+
+interface Mysql {
+  host: string
+  user: string
+  password: string
+  database: string
+  port?: number
+}
