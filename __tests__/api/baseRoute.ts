207f304700f7ebf8672a7249584b76fc8d941e1e
AlfieriChou <alfierichou@gmail.com>
2019-04-03 11:30:46 +0800

    basic route test

---
+++ __tests__/api/baseRoute.ts
@@ -0,0 +1,10 @@
+import * as request from 'supertest'
+import { app } from '../../src/app'
+
+describe('basic route tests!!!', () => {
+  test('get index route test!!', async done => {
+    const response = await request(app.callback()).get('/')
+    expect(response.status).toBe(200)
+    done()
+  })
+})
