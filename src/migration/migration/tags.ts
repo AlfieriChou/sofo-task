1ea59dda1bfb381cdd1e33f5a7a4e479da451716
AlfieriChou <alfierichou@gmail.com>
2019-04-03 15:41:09 +0800

    add tasks and tags migration

---
+++ src/migration/migration/tags.ts
@@ -0,0 +1,16 @@
+import { CreateTable } from '../type'
+import { Operation } from '../enum'
+
+export const createTag: CreateTable = {
+  opt: Operation.create,
+  table: 'sofo-tags',
+  column: {
+    id: { type: 'increments' },
+    user_id: { type: 'integer', comment: '关联用户' },
+    tag: { type: 'string', length: '16', comment: '标签' },
+    description: { type: 'text', comment: '标签描述' },
+    created_at: { type: 'timestamp', comment: '创建时间' },
+    updated_at: { type: 'datetime', comment: '更新时间' },
+    deleted_at: { type: 'datetime', comment: '删除时间' }
+  }
+}
---
+++ src/migration/migration/tasks.ts
@@ -0,0 +1,19 @@
+import { CreateTable } from '../type'
+import { Operation } from '../enum'
+
+export const createTask: CreateTable = {
+  opt: Operation.create,
+  table: 'sofo-tasks',
+  column: {
+    id: { type: 'increments' },
+    user_id: { type: 'integer', comment: '关联用户' },
+    tags: { type: 'string', length: '32', comment: '标签' },
+    content: { type: 'text', comment: '内容' },
+    date: { type: 'date', comment: '日期' },
+    start_at: { type: 'datetime', comment: '开始时间' },
+    end_at: { type: 'datetime', comment: '结束时间' },
+    created_at: { type: 'timestamp', comment: '创建时间' },
+    updated_at: { type: 'datetime', comment: '更新时间' },
+    deleted_at: { type: 'datetime', comment: '删除时间' }
+  }
+}
