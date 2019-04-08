4e86aaaa68258796047e4b605878c1eee54ea731
AlfieriChou <alfierichou@gmail.com>
2019-04-08 15:22:12 +0800

    add cors

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,32 @@ const redisSession: Middleware = session({
   }
 })
 
+const cors: Middleware = async (ctx, next) => {
+  if (ctx.request.method === 'OPTIONS') {
+    ctx.response.status = 200
+  }
+  ctx.set('Access-Control-Allow-Origin', ctx.request.header.origin)
+  ctx.set('Access-Control-Allow-Credentials', 'true')
+  ctx.set('Access-Control-Max-Age', '86400000')
+  ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE')
+  ctx.set(
+    'Access-Control-Allow-Headers',
+    'x-requested-with, accept, origin, content-type'
+  )
+  try {
+    await next()
+  } catch (err) {
+    ctx.status = err.statusCode || err.status || 500
+    ctx.body = {
+      code: ctx.status,
+      message: err.message,
+      stack: err.stack
+    }
+  }
+}
+
 app.use(redisSession)
+app.use(cors)
 if (process.env.NODE_ENV === 'development' || 'test') {
   app.use(logger())
 }
