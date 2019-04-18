4422097ce93a4e0d5a93435fc092c307af73ecee
AlfieriChou <alfierichou@gmail.com>
2019-04-18 11:17:05 +0800

    test: update user test

--- __tests__/api/user.ts
+++ __tests__/api/user.ts
@@ -3,20 +3,36 @@ import { app } from '../../src/app'
 
 describe('test user!!', () => {
   let token
-  beforeAll(async done => {
+  let id
+  test('test user register!!', async done => {
+    const response = await request(app.callback())
+      .post('/v1/register')
+      .send({
+        username: 'ddddd',
+        password: 'ddddddd',
+        age: 0,
+        description: 'haha'
+      })
+    id = response.body.id
+    expect(response.status).toBe(200)
+    done()
+  })
+
+  test('test user login!!', async done => {
     const response = await request(app.callback())
       .post('/v1/login')
       .send({
-        username: 'wang',
-        password: 'jundasdasd'
+        username: 'ddddd',
+        password: 'ddddddd'
       })
     token = response.body.token
+    expect(response.status).toBe(200)
     done()
   })
 
   test('test get user info!!', async done => {
     const response = await request(app.callback())
-      .get('/v1/users/1')
+      .get('/v1/users/' + id)
       .set('authorization', 'Bearer ' + token)
     expect(response.status).toBe(200)
     done()
@@ -24,11 +40,19 @@ describe('test user!!', () => {
 
   test('test update user info!!', async done => {
     const response = await request(app.callback())
-      .put('/v1/users/1')
+      .put('/v1/users/' + id)
       .set('authorization', 'Bearer ' + token)
       .send({
         description: 'haha'
       })
+    expect(response.status).toBe(200)
+    done()
+  })
+
+  test('test destroy user!!', async done => {
+    const response = await request(app.callback())
+      .delete('/v1/users/' + id)
+      .set('authorization', 'Bearer ' + token)
     expect(response.status).toBe(200)
     done()
   })
