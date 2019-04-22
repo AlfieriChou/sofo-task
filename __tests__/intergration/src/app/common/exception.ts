84bf746d63125c00a9e81142247098bde8270315
AlfieriChou <alfierichou@gmail.com>
2019-04-22 15:56:51 +0800

    test: exception test

---
+++ __tests__/intergration/src/app/common/exception.ts
@@ -0,0 +1,68 @@
+import {
+  ApiErrorException,
+  RuntimeException,
+  RestrictedAccessException,
+  NotAuthorizedException,
+  NotFoundException,
+  DataValidationException,
+  NotImplementedException,
+  Exception
+} from '../../../../../src/app/common/exception'
+
+describe('Test exception!!!', () => {
+  test('test ApiErrorException!!', async done => {
+    const t = () => {
+      throw new ApiErrorException('test', 400)
+    }
+    expect(t).toThrow(Exception)
+    done()
+  })
+
+  test('test RuntimeException!!', async done => {
+    const t = () => {
+      throw new RuntimeException('test')
+    }
+    expect(t).toThrow(Exception)
+    done()
+  })
+
+  test('test RestrictedAccessException!!', async done => {
+    const t = () => {
+      throw new RestrictedAccessException('test')
+    }
+    expect(t).toThrow(Exception)
+    done()
+  })
+
+  test('test NotAuthorizedException!!', async done => {
+    const t = () => {
+      throw new NotAuthorizedException('test')
+    }
+    expect(t).toThrow(Exception)
+    done()
+  })
+
+  test('test NotFoundException!!', async done => {
+    const t = () => {
+      throw new NotFoundException('test')
+    }
+    expect(t).toThrow(Exception)
+    done()
+  })
+
+  test('test DataValidationException!!', async done => {
+    const t = () => {
+      throw new DataValidationException('test')
+    }
+    expect(t).toThrow(Exception)
+    done()
+  })
+
+  test('test NotImplementedException!!', async done => {
+    const t = () => {
+      throw new NotImplementedException('test')
+    }
+    expect(t).toThrow(Exception)
+    done()
+  })
+})
